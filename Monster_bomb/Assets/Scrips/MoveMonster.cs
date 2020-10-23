50cfa418d8f6fd34084dada79c4472212a7f0c75
sarana <sarana.rb.ru@gmail.com>
2020-10-24 05:02:24 +0700

    создание BG и движение монстра

---
+++ Monster_bomb/Assets/Scrips/MoveMonster.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+public class MoveMonster : MonoBehaviour
+{
+    public Transform monster; // переменная типа трансформ (игрок)
+    [SerializeField] // свойство для того что прайвет виднелся в юнити
+    private float speed = 10f; //переменная скорости
+
+    void OnMouseDrag() // функция нажатия мышки и движение по объекту
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // переменная 
+        mousePos.x = mousePos.x > 2f ? 2f : mousePos.x; // проверка(если переменная больше то устанавление лимита)
+        mousePos.x = mousePos.x < -2f ? -2f : mousePos.x;
+        monster.position = Vector2.MoveTowards(monster.position, 
+            new Vector2(mousePos.x, monster.position.y),
+            speed * Time.deltaTime); // позиция игрока движение по горизонтали
+    }
+}
